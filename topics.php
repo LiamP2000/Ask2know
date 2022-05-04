3bd74856511fc2db18be5e5c6f9d7d0df5ed856a
LiamP2000 <liampeeters@live.be>
2022-05-04 16:33:27 +0200

    feat: basic frontend + checkbox test

---
+++ topics.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Choose your topics</title>
+</head>
+<body>
+
+    <h1>Topics</h1>
+    <p>Add the topics where you could help your colleagues with. You can always add more in the profile section.</p>
+
+     
+
+    <ul>
+        <li><input type="checkbox" id="cb1" />
+            <label for="cb1"><img src="images/chemical-icon-png-6.png" /></label>
+        </li>
+        <li><input type="checkbox" id="cb2" />
+            <label for="cb2"><img src="images/engineering.png" /></label>
+        </li>
+        <li><input type="checkbox" id="cb3" />
+            <label for="cb3"><img src="http://lorempixel.com/102/102" /></label>
+        </li>
+        <li><input type="checkbox" id="cb4" />
+            <label for="cb4"><img src="http://lorempixel.com/103/103" /></label>
+        </li>
+    </ul>
+
+    <a href="home.php">
+        <button>Confirm</button>
+    </a>
+</body>
+</html>
