8a3d0ef21083444080099221b81ad98675da7dc1
LiamP2000 <liampeeters@live.be>
2022-05-04 17:42:14 +0200

    feat: added basic frontend + navigation bar

---
+++ home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Home</title>
+</head>
+<body>
+    <h1>Hello [name]</h1>
+    <p>Maybe you can help with these issues?</p>
+
+    <div>
+        <p><strong>[name]</strong></p>
+        <p>[date]</p>
+        <p>[issue]</p>
+        <p>[amount of comments]</p>
+        <button>see more</button> <!-- detail.php? -->
+    </div>
+
+    <h2>Message from the company</h2>
+
+    <div class="navbar">
+        <a href="#home" class="active">Home</a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+    </div>
+</body>
+</html>
