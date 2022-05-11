eb1e1997fcb6c7c25e96d29cd8cbc8c24749e687
LiamP2000 <liampeeters@live.be>
2022-05-11 19:36:35 +0200

    feat: added basic frontend

---
+++ help.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="style.css">
+    <title>Home</title>
+</head>
+<body>
+
+    <nav>
+        <a href="home.php">Home</a>
+        <a href="help.php">Help</a>
+        <a href="profile.php">Profile</a>
+        <a href="messages.php">Messages</a>
+    </nav>
+
+    <input type="text" placeholder="Search..">
+
+    <p>[checklist met alle topics die geshowed moeten worden]</p>
+
+    <div class="">
+        <p><strong>[name]</strong></p>
+        <p>[date]</p>
+        <p>[issue]</p>
+        <p>[amount of comments]</p>
+        <button>see more</button> <!-- detail.php? -->
+    </div>
+
+    <a href="make_question.php">
+        <button class="">Make question</button>
+    </a>
+    
+
+
+    
+
+    
+</body>
+</html>
---
+++ make_question.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="style.css">
+    <title>Home</title>
+</head>
+<body>
+
+    <nav>
+        <a href="home.php">Home</a>
+        <a href="help.php">Help</a>
+        <a href="profile.php">Profile</a>
+        <a href="messages.php">Messages</a>
+    </nav>
+
+    <h2>What's the issue?</h2>
+    
+    <textarea name="issue" rows="4" cols="30" placeholder="Type here"></textarea>
+
+    <h3>Add image</h3>
+
+    <input type="file" name="image" id="image">
+
+    <h3>Add the topics related to the problem</h3>
+
+    <p>[checklist met alle topics die geshowed moeten worden]</p>
+
+    <a href="help.php">
+        <button class="">Post question</button>
+    </a>
+    
+
+
+    
+
+    
+</body>
+</html>
