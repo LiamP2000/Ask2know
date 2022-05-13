70f3ca90ef9473e44ceb64577481b823a590e578
LiamP2000 <liampeeters@live.be>
2022-05-13 16:14:45 +0200

    feat: added basic frontend

---
+++ settings.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="style.css">
+    <title>Home</title>
+</head>
+<body>
+
+    <nav>
+        <a href="home.php">Home</a>
+        <a href="help.php">Help</a>
+        <a href="profile.php">Profile</a>
+        <a href="messages.php">Messages</a>
+    </nav>
+
+    <a href="account.php">
+        <button>Account</button>
+    </a>
+
+    <a href="edit_topics.php"> 
+        <button>Topics</button>
+    </a>
+    
+    <a href="profile.php">
+        <img src="images/back-arrow.svg" alt="settings">
+    </a>
+    
+    
+
+
+    
+
+    
+</body>
+</html>
