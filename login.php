718928f48618ca80a5db828c8e27ccb737ac0613
LiamP2000 <liampeeters@live.be>
2022-05-13 16:24:24 +0200

    feat: comment what the conflict caused

--- login.php
+++ login.php
@@ -30,6 +30,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- bootstrap link gone due to merge conflict -->
     <link rel="stylesheet" href="style.css">
     <title>Log in</title>
 </head>
