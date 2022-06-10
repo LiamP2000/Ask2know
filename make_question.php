6b02d6a4bcf6cf5f5c3e370cbbcfdf5feff18708
LiamP2000 <liampeeters@live.be>
2022-06-10 20:50:32 +0200

    feat: upload question to database

--- make_question.php
+++ make_question.php
@@ -1,11 +1,21 @@
-<!DOCTYPE html>
+<?php
+
+    include_once("/autoloader.php");
+
+
+
+
+
+
+
+?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
-    <link rel="stylesheet" href="style.css">
+    
     <title>Home</title>
 </head>
 <body>
@@ -14,24 +24,35 @@
         <a href="home.php">Home</a>
         <a href="help.php">Help</a>
         <a href="profile.php">Profile</a>
-        <a href="messages.php">Messages</a>
+        <a href="chat.php">Messages</a>
     </nav>
 
-    <h2>What's the issue?</h2>
+    <form action="" method="post">
+        <div>
+            <label for="question">Question</label>
+            <input type="text" name="question" id="question">
+        </div>
+
+        <div>
+            <label for="image">Image</label>
+            <input type="file" name="image" id="image">
+        </div>
+
+        <div>
+            <label for="topics">Add the topics related to the problem</label>
+            <select name="topics" id="topics">
+                <option value="">Topic 1</option>
+                <option value="">Topic 2</option>
+                <option value="">Topic 3</option>
+        </div>
+
+
+        <div>
+            <input type="submit" name="submit" value="Post question">
+        </div>
+
+    </form>
     
-    <textarea name="issue" rows="4" cols="30" placeholder="Type here"></textarea>
-
-    <h3>Add image</h3>
-
-    <input type="file" name="image" id="image">
-
-    <h3>Add the topics related to the problem</h3>
-
-    <p>[checklist met alle topics die geshowed moeten worden]</p>
-
-    <a href="help.php">
-        <button class="">Post question</button>
-    </a>
     
 
 
