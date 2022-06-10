2b0c03ccd5b417516e0aae7764c2fe5036e6f3dd
LiamP2000 <liampeeters@live.be>
2022-06-10 20:48:09 +0200

    feat: navigation

---
+++ nav.inc.php
@@ -0,0 +1,8 @@
+<div class="navigation">  
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="help.php">Help</a></li>
+        <li><a href="profile.php">Profile</a></li>
+        <li><a href="chat.php">Chat</a></li>
+    </ul> 
+</div>
