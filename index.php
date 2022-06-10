f2cd93f25f7a6fd381c33b9376420110912a1bc7
LiamP2000 <liampeeters@live.be>
2022-06-10 20:44:53 +0200

    feat: styling navigation

--- index.php
+++ index.php
@@ -3,22 +3,50 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
-    <link rel="stylesheet" href="styles/styles.css">
-    <title>Opening screen</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./styles/style.css"> 
+    <title>Home</title>
 </head>
 <body>
-    <h1 class="header">Welcome to</h1>
+    
+<?php include_once(__DIR__ . "/nav.inc.php"); ?>
 
-    <img class="logo" src="images/logo.png" alt="logo"> <br>
+    <a href="login.php" class="btn">Log in</a> <br>
+    <a href="register.php" class="btn">Register</a>
+    
 
-    <div class="buttons">
-        <a href="login.php" class="primary__btn">Log in</a> <br>
-        <a href="register.php" class="btn">Register</a>
+    <h1>Hello [name]</h1>
+    <p>Maybe you can help with these issues?</p>
+
+    <div class="card">
+        <div class="name">
+            <p><strong>[name]</strong></p>
+        </div>
+        <div class="date">
+            <p>[date]</p>
+        </div>
+        <div class="question">
+            <p>[issue]</p>
+        </div>
+        <div class="see-more">
+            <a href="#" class="btn">see more</a> <!-- detail.php? -->
+        </div>
+        <div class="comments">
+            <p>[amount of comments]</p>
+        </div>
     </div>
-    
-    
 
+    <div>
+        
+        
+        
+        
+        
+    </div>
+
+    <h2>Message from the company</h2>
+    
 </body>
 </html>
