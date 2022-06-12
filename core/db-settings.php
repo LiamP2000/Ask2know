0fbc354c5788b81756c95edf2a361aa638ccbf71
LiamP2000 <liampeeters@live.be>
2022-06-12 13:26:40 +0200

    feat: autoloader and database settings

---
+++ core/db-settings.php
@@ -0,0 +1,10 @@
+<?php
+
+    const SETTINGS = [
+        "db" => [
+            "user" => "root",
+            "password" => "root",
+            "host" => "localhost",
+            "db" => "A2K"
+        ]
+    ];
