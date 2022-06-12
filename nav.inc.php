5a6444f6451730abe396b70e0703111d83046ec2
LiamP2000 <liampeeters@live.be>
2022-06-12 13:27:50 +0200

    fix: help is now feed

--- nav.inc.php
+++ nav.inc.php
@@ -1,7 +1,7 @@
 <div class="navigation">  
     <ul>
         <li><a href="index.php">Home</a></li>
-        <li><a href="feed.php">Help</a></li>
+        <li><a href="feed.php">Feed</a></li>
         <li><a href="profile.php">Profile</a></li>
         <li><a href="chat.php">Chat</a></li>
     </ul> 
